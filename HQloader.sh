ccf80fc0470e45c32bb8808a4ec99ec7a7f24c44
notscript <76538208+notscript@users.noreply.github.com>
2021-01-27 22:06:03 -0300

    Add files via upload

---
+++ HQloader.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+#Programas necessarios: Wget, Zip
+#Sistema Operacional necessario: Linux 
+
+
+
+echo "Digite o nome da wordlist:"
+read WDL 
+echo ""
+
+echo "Digite o nome da HQ:"
+read HQ
+echo ""
+
+sudo wget -nv --directory-prefix="$HQ" -i "$WDL"
+
+echo "
+DONE BITCH!
+DESEJA ZIPAR O DIRETORIO? (y/n)"
+read RESP
+
+if [ $RESP = "y" ]
+then 
+zip -r PolemicBooks.zip "$HQ"/
+echo "Compactado!"
+fi
+if [ $RESP = "n" ]
+then
+echo "OK, SAINDO..."
+sleep 3
+fi
+
+echo "
+Deseja apagar o diretorio?"
+read wish 
+
+if [ $wish = "y" ]
+then
+sudo rm -r -d "$HQ"/
+fi
+if [ $wish = "n" ]
+then
+echo "OK, SAINDO"
+sleep 3
+fi
+
+
+
